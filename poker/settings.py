d240efca65386d9df713d61a5fbbf2595a380ffd
Iván Nawrath <inawrath@smartboxtv.com>
2017-09-08 13:05:17 -0300

    Configuraciones -Se cambia DEBUG para servir archivos js

--- poker/settings.py
+++ poker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7jk9v4hqy08b*uy-xrgf_t7*p)wrrsfw+pa!+$7p-#@u5h*d--'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
